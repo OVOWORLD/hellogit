d9e2d7ba88d1b3c59ff00795cea4e07a3f43728b
OVOWORLD <OVOWORLD@users.noreply.github.com>
2018-01-30 15:17:32 -0400

    Ship.h

---
+++ Ship.h
@@ -0,0 +1,43 @@
+//Ship.h
+#include<string>
+using namespace std;
+
+class Ship
+{
+private:
+string Shipname;
+string Shiptype;
+float Maxspeed;
+
+public:
+Ship(string x , string y, float z)
+{
+setShipname(x);
+setShiptype(y);
+setMaxspeed(z);
+}
+void setShipname(string a)
+{
+Shipname=a;
+}
+string getShipname(){
+return Shipname;
+}
+void setShiptype(string b)
+{
+Shiptype=b;
+}
+string getShiptype(){
+return Shiptype;
+}
+void setMaxspeed(float c)
+{
+Maxspeed=c;
+}
+string getMaxspeed(){
+return Maxspeed;
+}
+};
+
+
+
